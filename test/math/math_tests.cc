e18216794ad14408a9b9d495a0f40c2562cbbf84
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-18 11:21:00 +0300

    cmake build for math testing

--- test/math/math_tests.cc
+++ test/math/math_tests.cc
@@ -1,46 +1,46 @@
 #include <iostream>
 #include <vector>
 
-#include "../../src/math/derivative.hh"
+// #include "../../src/math/derivative.hh"
 #include "../../src/math/math.hh"
 
-template <typename T>
-void printArray(T &array)
-{
-    for (auto &&n : array)
-        std::cout << n << " ";
-    std::cout << "\n";
-}
+// template <typename T>
+// void printArray(T &array)
+// {
+//     for (auto &&n : array)
+//         std::cout << n << " ";
+//     std::cout << "\n";
+// }
 
-void test_app()
-{
-    std::vector<double> v;
-    for (int i = 0; i < 10; ++i)
-    {
-        int xx = 1;
-        (i % 2 == 0) && v.emplace_back(i);
-    }
-    printArray<std::vector<double>>(v);
-}
+// void test_app()
+// {
+//     std::vector<double> v;
+//     for (int i = 0; i < 10; ++i)
+//     {
+//         int xx = 1;
+//         (i % 2 == 0) && v.emplace_back(i);
+//     }
+//     printArray<std::vector<double>>(v);
+// }
 
-double d2x(double x)
-{
-    return 2.0 * x;
-}
+// double d2x(double x)
+// {
+//     return 2.0 * x;
+// }
 
-void test_derivative()
-{
-    auto x = math::Derivative::generateFunctionArray(d2x);
-    for (auto &&n : x)
-    {
-        std::cout << "( " << n.x << " , " << n.y << " )"
-                  << "\n";
-    }
-    auto dx = math::Derivative::Differentiate_Array(x);
-    for (auto &&n : dx)
-        std::cout << n << " ";
-    std::cout << "\n";
-}
+// void test_derivative()
+// {
+//     auto x = math::Derivative::generateFunctionArray(d2x);
+//     for (auto &&n : x)
+//     {
+//         std::cout << "( " << n.x << " , " << n.y << " )"
+//                   << "\n";
+//     }
+//     auto dx = math::Derivative::Differentiate_Array(x);
+//     for (auto &&n : dx)
+//         std::cout << n << " ";
+//     std::cout << "\n";
+// }
 
 void test_mathclass()
 {
